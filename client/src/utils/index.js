e846cf9898f8d33100d181f8e22f42e834c19b8d
aaliyahsymlar <asym14@gmail.com>
2023-06-01 12:26:12 -0500

    basic structures of app

---
+++ client/src/utils/index.js
@@ -0,0 +1,11 @@
+import { surpriseMePrompts } from "../constants";
+
+// every time this function is called it will generate a prompt from the constants array
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
